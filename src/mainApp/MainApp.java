1edabf2c10e57ad4b2f1f033a39a0459d7626295
rhit_pereirl <pereirl@rose-hulman.edu>
2024-01-30 22:10:47 -0500

    All Barriers, Coins, and the Hero appear on the screen. Furthermore, hero is capable of horizontal and vertical movement.

--- src/mainApp/MainApp.java
+++ src/mainApp/MainApp.java
@@ -1,10 +1,14 @@
 package mainApp;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
+
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 /**
  * Class: MainApp
@@ -22,10 +26,26 @@ public class MainApp {
 		JFrame frame = new JFrame();
 		frame.setSize(1000, 400);
 		frame.setTitle("Jetpack Joyride");
-		//System.out.println(filename);
+		System.out.println(filename);
 		Level level1 = new Level(filename);
 		frame.add(level1);
 		
+		Timer t = new Timer(50, new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+				advanceOneTick();
+				
+			}
+			public void advanceOneTick() {
+				level1.updateState();
+				level1.repaint();
+			}
+		});
+		
+		t.start();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -35,25 +55,19 @@ public class MainApp {
 	 * ensures: runs the application
 	 * @param args unused
 	 */
-	public static void main(String[] args) throws IOException {
+	
+	
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 		MainApp mainApp = new MainApp();
-		Scanner s = new Scanner(System.in);
-		String filename = null;
-		boolean isDone = false;
 		
-		while (!isDone) {
-			try {
-				System.out.println("What level should I load?");
-				filename = s.nextLine();
-				runApp(filename);
-				isDone = true;
-			} catch (FileNotFoundException e) {
-				System.err.println("Level " + filename + " not found");
-				System.out.println(e);
-			}
+		try {
+			runApp("trialRun.txt");
+		} catch (FileNotFoundException e) {
+			System.err.println("File Not Found :(");
 		}
-		s.close();
-		System.out.println("Game Closed");
+		
+		
 				
 	} // main
 
