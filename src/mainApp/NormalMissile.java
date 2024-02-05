1c685ea109976d76aeb69c06806e592241a9d25c
goikwj <goikwj@rose-hulman.edu>
2024-02-05 14:46:06 -0500

    Adding normal missiles and gravity

---
+++ src/mainApp/NormalMissile.java
@@ -0,0 +1,14 @@
+package mainApp;
+
+import java.awt.Color;
+
+public class NormalMissile extends Missile {
+	
+	public NormalMissile(int xCoord, int yCoord) {
+		super(xCoord, yCoord, Color.MAGENTA);
+	}
+	
+	
+	
+	
+}
---
+++ src/mainApp/TrackingMissile.java
@@ -0,0 +1,10 @@
+package mainApp;
+
+import java.awt.Color;
+
+public class TrackingMissile extends Missile {
+	
+	public TrackingMissile(int xCoord, int yCoord) {
+		super(xCoord, yCoord, Color.DARK_GRAY);
+	}
+}
